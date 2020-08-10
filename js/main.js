2ae77aac734a9699b9add5922e80d4b999737aaf
Larisa-Levchenko <larisa.levchenko.98@mail.ru>
2020-08-10 20:30:28 +0700

    feat in js/main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
-let money = 30000;
+'use strict';
+
+let money = prompt('Ваш месячный доход?');
 let income = 'фриланс';
-let addExpenses = 'Продукты, Комуналка, Интернет, Шопинг';
-let deposit = true;
+let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+let deposit = confirm('Есть ли у вас депозит в банке?');
+let expenses1 = prompt('Введите обязательную статью расходов?');
+let amount1 = prompt('Во сколько это обойдется?');
+let expenses2 = prompt('Введите обязательную статью расходов?');
+let amount2 = prompt('Во сколько это обойдется?');
 let mission = 100000;
 let period = 6;
 
@@ -16,6 +22,19 @@ console.log('Цель заработать', mission, 'рублей');
 
 console.log(addExpenses.toLowerCase().split(' '));
 
+let budgetMonth = money - amount1 - amount2;
+console.log('Бюджет на месяц: ', budgetMonth);
 
-let budgetDay = money/30;
-console.log(budgetDay);
+let period2 = mission/budgetMonth;
+console.log('Цель будет достугнута за:',Math.ceil(period2), 'месяцев');
+let budgetDay = budgetMonth/30;
+console.log(Math.floor(budgetDay));
+if(budgetDay>=1200){
+    console.log('У вас высокий уровень дохода');
+}else if(budgetDay>=600){
+    console.log('У вас средний уровень дохода');
+}else if(budgetDay<0){
+    console.log('Что то пошло не так');    
+}else {
+    console.log('К сожалению у вас уровень дохода ниже среднего');
+}
